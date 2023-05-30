bb6f5075eab1a69067f23dc699bbaa9fdb06f434
lb2506 <leo.baccialone@laposte.net>
2023-05-30 20:35:19 +0200

    const url to switch with branches

--- frontend/src/components/admin/addBook/addBook.jsx
+++ frontend/src/components/admin/addBook/addBook.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
-import { url } from '../../api';
+import { url } from '../../../api';
 
 
 const AddBook = () => {
--- frontend/src/components/admin/adminPanel/adminPanel.jsx
+++ frontend/src/components/admin/adminPanel/adminPanel.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
-import { url } from '../../api';
+import { url } from '../../../api';
 import "./adminPanel.scss"
 
 const AdminPanel = () => {

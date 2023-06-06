b2228fcc1636ad94ed2690bcc0bc315729c8619b
lb2506 <leo.baccialone@laposte.net>
2023-06-06 12:08:02 +0200

    change to url render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-export const url = "http://localhost:5000";
+// export const url = "http://localhost:5000";
 export const url_api_librairie = "https://api-librairie.onrender.com";
 
-// export const url = "https://books-zpg6.onrender.com";
+export const url = "https://books-zpg6.onrender.com";
